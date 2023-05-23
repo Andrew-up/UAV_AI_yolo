892d41a37e46c9d3d3c9e417889e022173759dcc
Andrew-up <aageew@bk.ru>
2023-05-23 22:35:41 +0300

    123

--- main.py
+++ main.py
@@ -3,27 +3,29 @@ from ultralytics import YOLO
 
 def print_hi(name):
     # print(torch.version.cuda)
-    # # Load a model
-    model = YOLO('yolov8s.yaml')  # build a new model from scratch
-    model = YOLO('yolov8s.pt')  # load a pretrained model (recommended for training)
-    #
-    #
-    # # model.export(format='pb')
-    # # Use the model
+
+    # model = YOLO('yolov8s.yaml')  # build a new model from scratch
+    # model = YOLO('yolov8s.pt')  # load a pretrained model (recommended for training)
+
     # results = model.train(data="data_yaml.yaml", epochs=5, imgsz=512, name='yolov8s_custom',
     #                       batch=32)  # train the model
-    # wandb.finish()
-    # results = model('https://ultralytics.com/images/bus.jpg')  # predict on an image
-    # model = YOLO('runs/detect/yolov8s_custom3/weights/best.pt')
 
-    model.export(format='onnx')
+
+    model = YOLO('runs/detect/yolov8s_custom/weights/best.pt')
+
+    # model.export(format='onnx')
 
 
     # art = wandb.Artifact("my-object-detector", type="model")
     # art.add_file("runs/detect/yolov8s_custom/weights/best.pt")
     # wandb.log_artifact(art)
 
-    # model.predict('testImg', save=True, imgsz=512, conf=0.3)
+    results = model('test_data/images/1_001541.JPG', conf=0.1)
+
+    print(results[0].boxes)
+    print(results[0].boxes.conf)
+    # print(results.probs)
+
 
     # pl
     # success = model.export(format='onnx')  # export the model to ONNX format
