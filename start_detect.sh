ec46b87302cf8083a3ba05e1531575d23715926e
Andrew-up <67470168+Andrew-up@users.noreply.github.com>
2023-09-22 14:17:00 +0300

    upd

---
+++ start_detect.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "hello world"
+source /home/user/anaconda3/etc/profile.d/conda.sh
+conda activate uav_ai
+conda info
+python -V
+python test_model.py
+read -p "Press enter to continue"
+gnome-terminal &   # launch a new terminal
+kill $PPID         # kill this
+
