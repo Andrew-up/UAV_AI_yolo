630d6ae5e193c3e87a94d4e987daf4a12ed93dba
Andrew-up <67470168+Andrew-up@users.noreply.github.com>
2023-09-24 10:50:43 +0300

    upd

--- start_train.sh
+++ start_train.sh
@@ -4,7 +4,7 @@ source /home/user/anaconda3/etc/profile.d/conda.sh
 conda activate uav_ai
 conda info
 python -V
-# python main.py
+python main.py
 read -p "Press enter to continue"
 gnome-terminal &   # launch a new terminal
 kill $PPID         # kill this
